d5510e01805a3bf84980d46162904daf0ed611c3
Peter Marangoni <91223500+Petalon@users.noreply.github.com>
2024-12-16 18:57:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-function showConfetti() {
+function showConfetti(event) {
+  const button = event.currentTarget;
+  const rect = button.getBoundingClientRect();
+  const x = rect.left + rect.width / 2;
+  const y = rect.top + rect.height / 2;
+
   confetti({
     particleCount: 150,
     spread: 60,
     shapes: ['star'], // Custom shape for particles
     colors: ['#FFD700'], // Color for dollar sign particles
+    origin: { x: x / window.innerWidth, y: y / window.innerHeight }
   });
 }
