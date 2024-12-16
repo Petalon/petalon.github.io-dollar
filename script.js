acbca34084f64c57b294376867729e59663b5e97
Peter Marangoni <91223500+Petalon@users.noreply.github.com>
2024-12-16 18:48:43 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function showConfetti() {
+  confetti({
+    particleCount: 150,
+    spread: 60,
+    shapes: ['star'], // Custom shape for particles
+    colors: ['#FFD700'], // Color for dollar sign particles
+  });
+}
